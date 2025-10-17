fc620ad06ca8e5740621e976b58836e6a6c9bd63
Alexey Lazarev <lexalazarev2002@yandex.ru>
2025-10-17 17:03:11 +0300

    Add main window, menu bar

---
+++ app.py
@@ -0,0 +1,72 @@
+import sys
+from PySide6.QtWidgets import QApplication, QMenuBar, QMainWindow, QMenu, QTableWidget, QWidget, QVBoxLayout
+from PySide6.QtCore import QSize, QSettings
+from PySide6.QtGui import QKeySequence
+
+
+class MainWindow(QMainWindow):
+    def __init__(self) -> QMainWindow:
+        super().__init__(
+            size=QSize(600, 800),
+            minimumSize=QSize(400, 400),
+            windowTitle='FmgEditor'
+        )
+
+        self.settings = QSettings('Exsodium', 'FmgEditor')
+        geometry = self.settings.value('geometry')
+
+        if geometry:
+            self.restoreGeometry(geometry)
+
+        menu_bar = QMenuBar()
+        file_menu = Menu(self)
+        menu_bar.addMenu(file_menu)
+        self.setMenuBar(menu_bar)
+
+    def closeEvent(self, event) -> None:
+        self.settings.setValue('geometry', self.saveGeometry())
+        super().closeEvent(event)
+
+
+class Menu(QMenu):
+    def __init__(self, parent: MainWindow) -> QMenu:
+        super().__init__('&Файл', parent=parent)
+        self.addAction(
+            'Открыть файл...', self._on_open_file, shortcut=QKeySequence.StandardKey.Open)
+        self.addAction(
+            'Сохранить', self._on_save_file, shortcut=QKeySequence.StandardKey.Save)
+
+    def _on_open_file(self) -> None:
+        central_widget = QWidget()
+        layout = QVBoxLayout(central_widget)
+        table = Table()
+        layout.addWidget(table)
+
+        parent: MainWindow = self.parent()
+        parent.setCentralWidget(central_widget)
+
+    def _on_save_file(self) -> None:
+        print(QKeySequence.StandardKey.Save)
+
+
+class Table(QTableWidget):
+    def __init__(self) -> QTableWidget:
+        super().__init__()
+        self.verticalHeader().setVisible(False)
+        self.setColumnCount(2)
+        self.setRowCount(1)
+        self.setHorizontalHeaderLabels(['ID', 'Текст'])
+        self.resizeColumnsToContents()
+        self.horizontalHeader().setMinimumSectionSize(40)
+        self.horizontalHeader().setFixedHeight(20)
+        self.verticalHeader().setMinimumSectionSize(12)
+        self.verticalHeader().setDefaultSectionSize(20)
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec())
