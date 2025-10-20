d00ad3c292774c568ddbe71b6ae2a77a103441b2
Alexey Lazarev <lexalazarev2002@yandex.ru>
2025-10-20 18:51:57 +0300

    Add ability to save file

--- package/functions.py
+++ package/functions.py
@@ -1,5 +1,6 @@
-from io import BufferedReader
+from io import BufferedReader, BufferedWriter
 import struct
+from os import fstat
 
 
 def read_file(file_path: str) -> dict[str, str]:
@@ -60,3 +61,104 @@ def read_unicode_string(file: BufferedReader, offset: int) -> str:
             break
 
     return data.decode('utf-16-le') if data else ''
+
+
+def write_data_to_file(data: dict[str, str], file_path: str) -> None:
+    with open(file_path, 'wb') as file:
+        entries_amount = 0
+        previous_id = -2
+        chunks_amount = 0
+
+        for id in data.keys():
+            current_id = int(id)
+
+            if current_id > previous_id + 1:
+                chunks_amount += 1
+
+            entries_amount += 1
+            previous_id = current_id
+
+        start_offset = 0x1c + 0xC * chunks_amount
+        text_offset = start_offset + entries_amount * 4
+
+        first_id = int(list(data.keys())[0])
+        last_id = first_id
+        start_entry = 0
+        entries_amount = 0
+        chunks_amount = 0
+
+        for id, text in data.items():
+            current_id = int(id)
+
+            if current_id > last_id + 1:
+                entry_offset = 0x1C + chunks_amount * 0xC
+                write_int(file=file, int_type=32,
+                          offset=entry_offset, value=start_entry)
+                write_int(file=file, int_type=32,
+                          offset=entry_offset + 4, value=first_id)
+                write_int(file=file, int_type=32,
+                          offset=entry_offset + 8, value=last_id)
+
+                first_id = current_id
+                start_entry = entries_amount
+                chunks_amount += 1
+
+            string = text.replace('\r\n', '')
+
+            if string:
+                write_int(
+                    file=file,
+                    int_type=32,
+                    offset=start_offset + entries_amount * 4,
+                    value=text_offset
+                )
+
+                string = string.replace('/n/', '\n')
+
+                if not string.endswith('\0'):
+                    string += '\0'
+
+                write_unicode_string(file, text_offset, string)
+                text_offset += len(string) * 2
+
+            entries_amount += 1
+            last_id = current_id
+
+        st_size = fstat(file.fileno()).st_size
+        if st_size % 4 == 2:
+            write_int(file=file, int_type=16, offset=text_offset, value=0)
+
+        write_int(file=file, int_type=32,
+                  offset=entry_offset, value=start_entry)
+        write_int(file=file, int_type=32,
+                  offset=entry_offset + 4, value=first_id)
+        write_int(file=file, int_type=32,
+                  offset=entry_offset + 8, value=last_id)
+
+        write_int(file=file, int_type=32, offset=0, value=0x10000)
+        write_int(file=file, int_type=8, offset=0x8, value=1)
+
+        write_int(file=file, int_type=32, offset=0x4, value=st_size)
+        write_int(file=file, int_type=32, offset=0xC, value=chunks_amount + 1)
+        write_int(file=file, int_type=32, offset=0x10, value=entries_amount)
+        write_int(file=file, int_type=32, offset=0x14, value=start_offset)
+
+
+def write_int(file: BufferedWriter, int_type: int, offset: int, value: int) -> None:
+    file.seek(offset)
+
+    match int_type:
+        case 8:
+            format = 'b'
+        case 16:
+            format = '<h'
+        case 32:
+            format = '<i'
+
+    file.write(struct.pack(format, value))
+
+
+def write_unicode_string(file: BufferedWriter, offset: int, string: str) -> None:
+    file.seek(offset)
+    encoded_string = string.encode('utf-16-le')
+    file.write(encoded_string)
--- package/widgets.py
+++ package/widgets.py
@@ -1,11 +1,11 @@
 from PySide6.QtWidgets import (QMenuBar, QMainWindow, QMenu, QTableWidget,
                                QWidget, QVBoxLayout, QFileDialog,
                                QTableWidgetItem, QStyledItemDelegate,
-                               QPlainTextEdit)
+                               QPlainTextEdit, QHeaderView)
 from PySide6.QtCore import (QSize, QSettings, Qt, QModelIndex,
                             QAbstractItemModel, QRect)
-from PySide6.QtGui import QKeySequence
-from package.functions import read_file
+from PySide6.QtGui import QKeySequence, QAction
+from package.functions import read_file, write_data_to_file
 
 
 class MainWindow(QMainWindow):
@@ -37,6 +37,9 @@ class MainWindow(QMainWindow):
     def add_file_name_to_window_title(self, file_name: str) -> None:
         self.setWindowTitle(f'{file_name} - {self.window_title}')
 
+    def add_dot_to_window_title(self) -> None:
+        self.setWindowTitle(f'• {self.windowTitle()}')
+
 
 class Menu(QMenu):
     def __init__(self, parent: MainWindow) -> QMenu:
@@ -44,10 +47,12 @@ class Menu(QMenu):
         self.main_window = parent
         self.addAction(
             'Открыть файл...', self._on_open_file, shortcut=QKeySequence.StandardKey.Open)
-        self.addAction(
+        self.save_action: QAction = self.addAction(
             'Сохранить', self._on_save_file, shortcut=QKeySequence.StandardKey.Save)
         self.addSeparator()
         self.addAction('Выход', self._on_exit)
+
+        self.save_action.setEnabled(False)
 
     def _on_open_file(self) -> None:
         last_directory = self.main_window.settings.value('last_directory')
@@ -58,7 +63,7 @@ class Menu(QMenu):
             dir=last_directory
         )
 
-        if not file_path:
+        if file_path.isEmpty():
             return
 
         file_name = file_path.fileName()
@@ -69,42 +74,58 @@ class Menu(QMenu):
 
         central_widget = QWidget()
         layout = QVBoxLayout(central_widget)
-        table = Table()
-        layout.addWidget(table)
+        self.table = Table()
+        layout.addWidget(self.table)
         self.main_window.setCentralWidget(central_widget)
 
-        file_path = file_path.path()[1:]
-        table.load_data_from_file(file_path)
-        table.resizeRowsToContents()
-        table.resizeColumnsToContents()
+        self.file_path = file_path.path()[1:]
+        self.table.load_data_from_file(self.file_path)
+        self.table.resizeRowsToContents()
+        self.table.resizeColumnsToContents()
+
+        self.save_action.setEnabled(False)
 
     def _on_save_file(self) -> None:
-        print(QKeySequence.StandardKey.Save)
+        data = {}
+
+        for i in range(self.table.rowCount()):
+            id = self.table.item(i, 0).text()
+            text = self.table.item(i, 1).text()
+
+            data[id] = text
+
+        write_data_to_file(data, self.file_path)
+
+        self.main_window.setWindowTitle(self.main_window.windowTitle()[2:])
+        self.save_action.setEnabled(False)
 
     def _on_exit(self) -> None:
         self.main_window.close()
+
+    def set_save_action_enabled(self) -> None:
+        self.save_action.setEnabled(True)
 
 
 class Table(QTableWidget):
     def __init__(self) -> QTableWidget:
         super().__init__()
-        self.verticalHeader().setVisible(False)
         self.setColumnCount(2)
         self.setHorizontalHeaderLabels(['ID', 'Текст'])
-        self.resizeColumnsToContents()
+        self.verticalHeader().setVisible(False)
         self.horizontalHeader().setMinimumSectionSize(40)
         self.horizontalHeader().setFixedHeight(20)
         self.horizontalHeader().setStretchLastSection(True)
+
         self.verticalHeader().setMinimumSectionSize(12)
         self.verticalHeader().setDefaultSectionSize(20)
-        self.setWordWrap(True)
-        # self.verticalHeader().setSectionResizeMode(
-        #     QHeaderView.ResizeMode.ResizeToContents)
+
         self.setVerticalScrollMode(QTableWidget.ScrollMode.ScrollPerPixel)
         self.setHorizontalScrollMode(QTableWidget.ScrollMode.ScrollPerPixel)
-        self.setStyleSheet("QHeaderView::section{background-color:Snow}")
-        self.setItemDelegate(PlainTextEditDelegate())
-        self.setTextElideMode(Qt.TextElideMode.ElideNone)
+        self.verticalHeader().setSectionResizeMode(
+            QHeaderView.ResizeMode.ResizeToContents)
+
+        self.setStyleSheet('QHeaderView::section{background-color:Snow}')
+        self.setItemDelegate(PlainTextEditDelegate(self))
 
     def load_data_from_file(self, file_path: str) -> None:
         data = read_file(file_path)
@@ -131,12 +152,24 @@ class PlainTextEditDelegate(QStyledItemDelegate):
         value = index.data(Qt.ItemDataRole.EditRole)
         editor.setPlainText(str(value))
 
-    def setModelData(self, editor: QPlainTextEdit, model: QAbstractItemModel, index):
-        model.setData(index, editor.toPlainText(), Qt.ItemDataRole.EditRole)
+    def setModelData(self, editor: QPlainTextEdit, model: QAbstractItemModel, index: QModelIndex):
+        old_value: str = index.data(Qt.ItemDataRole.EditRole)
+        new_value = editor.toPlainText()
+
+        model.setData(index, new_value, Qt.ItemDataRole.EditRole)
+        main_window: MainWindow = self.parent().parent().parent()
+        menu: Menu = main_window.menuWidget().actions()[0].menu()
+
+        if old_value != new_value:
+            if not ('•' in main_window.windowTitle()):
+                main_window.add_dot_to_window_title()
+            menu.set_save_action_enabled()
+        else:
+            print('Same')
 
     def updateEditorGeometry(self, editor: QPlainTextEdit, option, index):
         rect: QRect = option.rect
-        rect.setHeight(100)
+        rect.setHeight(rect.height() + 10)
         editor.setGeometry(rect)
 
     def _create_key_press_handler(self, editor: QPlainTextEdit):
