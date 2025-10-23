3c00075d39a4b3123b9406498a235167c436474c
Alexey Lazarev <lexalazarev2002@yandex.ru>
2025-10-23 17:44:10 +0300

    Add localization files

---
+++ translations/en.ts
@@ -0,0 +1,84 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>File dialog</name>
+    <message>
+        <location filename="package/widgets.py" line="172"/>
+        <source>Open file</source>
+        <translation>Open file</translation>
+    </message>
+</context>
+<context>
+    <name>File menu</name>
+    <message>
+        <location filename="package/widgets.py" line="228"/>
+        <source>&amp;File</source>
+        <translation>&amp;File</translation>
+    </message>
+    <message>
+        <location filename="package/widgets.py" line="231"/>
+        <source>&amp;Open</source>
+        <translation>&amp;Open</translation>
+    </message>
+    <message>
+        <location filename="package/widgets.py" line="233"/>
+        <source>&amp;Save</source>
+        <translation>&amp;Save</translation>
+    </message>
+    <message>
+        <location filename="package/widgets.py" line="235"/>
+        <source>&amp;Exit</source>
+        <translation>&amp;Exit</translation>
+    </message>
+</context>
+<context>
+    <name>Languages menu</name>
+    <message>
+        <location filename="package/widgets.py" line="144"/>
+        <source>&amp;Language</source>
+        <translation>&amp;Language</translation>
+    </message>
+    <message>
+        <location filename="package/widgets.py" line="147"/>
+        <source>&amp;English</source>
+        <translation>&amp;English</translation>
+    </message>
+    <message>
+        <location filename="package/widgets.py" line="149"/>
+        <source>&amp;Russian</source>
+        <translation>&amp;Russian</translation>
+    </message>
+</context>
+<context>
+    <name>Message box</name>
+    <message>
+        <location filename="package/widgets.py" line="44"/>
+        <source>Text</source>
+        <translation>Want to saves your changes to</translation>
+    </message>
+    <message>
+        <location filename="package/widgets.py" line="50"/>
+        <source>&amp;Save</source>
+        <translation>&amp;Save</translation>
+    </message>
+    <message>
+        <location filename="package/widgets.py" line="52"/>
+        <source>&amp;Don&apos;t Save</source>
+        <translation>&amp;Don&apos;t Save</translation>
+    </message>
+    <message>
+        <location filename="package/widgets.py" line="54"/>
+        <source>&amp;Cancel</source>
+        <translation>&amp;Cancel</translation>
+    </message>
+</context>
+<context>
+    <name>Table headers</name>
+    <message>
+        <location filename="package/widgets.py" line="284"/>
+        <source>Text</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
---
+++ translations/ru.ts
@@ -0,0 +1,84 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>File dialog</name>
+    <message>
+        <location filename="package/widgets.py" line="172"/>
+        <source>Open file</source>
+        <translation>Открыть файл</translation>
+    </message>
+</context>
+<context>
+    <name>File menu</name>
+    <message>
+        <location filename="package/widgets.py" line="228"/>
+        <source>&amp;File</source>
+        <translation>&amp;Файл</translation>
+    </message>
+    <message>
+        <location filename="package/widgets.py" line="231"/>
+        <source>&amp;Open</source>
+        <translation>&amp;Открыть</translation>
+    </message>
+    <message>
+        <location filename="package/widgets.py" line="233"/>
+        <source>&amp;Save</source>
+        <translation>&amp;Сохранить</translation>
+    </message>
+    <message>
+        <location filename="package/widgets.py" line="235"/>
+        <source>&amp;Exit</source>
+        <translation>&amp;Выход</translation>
+    </message>
+</context>
+<context>
+    <name>Languages menu</name>
+    <message>
+        <location filename="package/widgets.py" line="144"/>
+        <source>&amp;Language</source>
+        <translation>&amp;Язык</translation>
+    </message>
+    <message>
+        <location filename="package/widgets.py" line="147"/>
+        <source>&amp;English</source>
+        <translation>&amp;Английский</translation>
+    </message>
+    <message>
+        <location filename="package/widgets.py" line="149"/>
+        <source>&amp;Russian</source>
+        <translation>&amp;Русский</translation>
+    </message>
+</context>
+<context>
+    <name>Message box</name>
+    <message>
+        <location filename="package/widgets.py" line="44"/>
+        <source>Text</source>
+        <translation>Вы хотите сохранить изменения в</translation>
+    </message>
+    <message>
+        <location filename="package/widgets.py" line="50"/>
+        <source>&amp;Save</source>
+        <translation>&amp;Сохранить</translation>
+    </message>
+    <message>
+        <location filename="package/widgets.py" line="52"/>
+        <source>&amp;Don&apos;t Save</source>
+        <translation>&amp;Не сохранять</translation>
+    </message>
+    <message>
+        <location filename="package/widgets.py" line="54"/>
+        <source>&amp;Cancel</source>
+        <translation>&amp;Отмена</translation>
+    </message>
+</context>
+<context>
+    <name>Table headers</name>
+    <message>
+        <location filename="package/widgets.py" line="284"/>
+        <source>Text</source>
+        <translation>Текст</translation>
+    </message>
+</context>
+</TS>
