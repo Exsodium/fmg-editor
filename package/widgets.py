f93fe758390422f1a0e4cfd1aadd1feb0bf83e1c
Alexey Lazarev <lexalazarev2002@yandex.ru>
2025-10-23 17:45:46 +0300

    Add localization, save file on close

--- package/widgets.py
+++ package/widgets.py
@@ -1,10 +1,12 @@
 from PySide6.QtWidgets import (QMenuBar, QMainWindow, QMenu, QTableWidget,
                                QWidget, QVBoxLayout, QFileDialog,
                                QTableWidgetItem, QStyledItemDelegate,
-                               QPlainTextEdit, QHeaderView)
+                               QPlainTextEdit, QHeaderView, QApplication,
+                               QMessageBox, QPushButton)
 from PySide6.QtCore import (QSize, QSettings, Qt, QModelIndex,
-                            QAbstractItemModel, QRect)
-from PySide6.QtGui import QKeySequence, QAction
+                            QAbstractItemModel, QRect, QCoreApplication,
+                            QTranslator)
+from PySide6.QtGui import QKeySequence, QAction, QActionGroup
 from package.functions import read_file, write_data_to_file
 
 
@@ -25,16 +27,76 @@ class MainWindow(QMainWindow):
         if geometry:
             self.restoreGeometry(geometry)
 
+        self.translator = QTranslator()
+        self.language = self.settings.value('language', 'en')
+
         menu_bar = QMenuBar()
-        file_menu = FileMenu(self)
-        language_menu = LanguageMenu()
-        menu_bar.addMenu(file_menu)
+        self.file_menu = FileMenu(self)
+        language_menu = LanguageMenu(self)
+        menu_bar.addMenu(self.file_menu)
         menu_bar.addMenu(language_menu)
         self.setMenuBar(menu_bar)
 
     def closeEvent(self, event) -> None:
+        window_title = self.windowTitle()
+        if '•' in window_title:
+            file_name = window_title[2:window_title.find('.fmg') + 4]
+            message_box = QMessageBox(
+                parent=self,
+                text=f'{QApplication.translate('Message box', 'Text')} {file_name}?',
+                icon=QMessageBox.Icon.Warning
+            )
+            message_box.setWindowTitle(self.window_title)
+
+            save_button = QPushButton(
+                QApplication.translate('Message box', '&Save'))
+            discard_button = QPushButton(
+                QApplication.translate('Message box', '&Don\'t Save'))
+            cancel_button = QPushButton(
+                QApplication.translate('Message box', '&Cancel'))
+
+            message_box.addButton(
+                save_button, QMessageBox.ButtonRole.AcceptRole)
+            message_box.addButton(
+                discard_button, QMessageBox.ButtonRole.DestructiveRole)
+            message_box.addButton(
+                cancel_button, QMessageBox.ButtonRole.RejectRole)
+            message_box.setDefaultButton(save_button)
+
+            message_box.exec()
+
+            clicked_button = message_box.clickedButton()
+
+            if clicked_button == save_button:
+                self.file_menu._on_save_file()
+            elif clicked_button == discard_button:
+                pass
+            else:
+                event.ignore()
+                return
+
         self.settings.setValue('geometry', self.saveGeometry())
         super().closeEvent(event)
+
+    def apply_language(self, language_code: str) -> None:
+        QApplication.removeTranslator(self.translator)
+
+        if self.translator.load(f'translations\\{language_code}.qm'):
+            QApplication.installTranslator(self.translator)
+            self.language = language_code
+
+            self.settings.setValue('language', language_code)
+            self.retranslate_ui()
+
+    def retranslate_ui(self) -> None:
+        for action in self.menuBar().actions():
+            action.menu().retranslate_menu()
+
+        central_widget = self.centralWidget()
+
+        if central_widget:
+            table: Table = self.centralWidget().children()[-1]
+            table.retranslate_table()
 
     def add_file_name_to_window_title(self, file_name: str) -> None:
         self.setWindowTitle(f'{file_name} - {self.window_title}')
@@ -43,24 +105,73 @@ class MainWindow(QMainWindow):
         self.setWindowTitle(f'• {self.windowTitle()}')
 
 
+class LanguageMenu(QMenu):
+    def __init__(self, parent: MainWindow) -> QMenu:
+        super().__init__(parent)
+        self.main_window = parent
+        language_group = QActionGroup(self)
+        language_group.setExclusive(True)
+
+        language_codes = ['en', 'ru']
+
+        for language_code in language_codes:
+            action = self.addAction(str())
+            action.setCheckable(True)
+            action.setData(language_code)
+            language_group.addAction(action)
+
+            if language_code == self.main_window.settings.value('language'):
+                action.setChecked(True)
+
+            action.triggered.connect(self._on_language_selected)
+
+        self.apply_language(self.main_window.settings.value('language'))
+
+    def _on_language_selected(self) -> None:
+        action = self.sender()
+
+        if action and action.isChecked():
+            language_code = action.data()
+            self.main_window.apply_language(language_code)
+
+    def apply_language(self, language_code: str) -> None:
+        translator = self.main_window.translator
+        QApplication.removeTranslator(translator)
+
+        if translator.load(f'translations\\{language_code}.qm'):
+            QApplication.installTranslator(translator)
+            self.retranslate_menu()
+
+    def retranslate_menu(self) -> None:
+        self.setTitle(QCoreApplication.translate(
+            'Languages menu', '&Language'))
+        lang_actions = self.actions()
+        lang_actions[0].setText(QCoreApplication.translate(
+            'Languages menu', '&English'))
+        lang_actions[1].setText(QCoreApplication.translate(
+            'Languages menu', '&Russian'))
+
+
 class FileMenu(QMenu):
     def __init__(self, parent: MainWindow) -> QMenu:
-        super().__init__('&Файл', parent=parent)
+        super().__init__(parent)
         self.main_window = parent
-        self.addAction(
-            'Открыть файл...', self._on_open_file, shortcut=QKeySequence.StandardKey.Open)
-        self.save_action: QAction = self.addAction(
-            'Сохранить', self._on_save_file, shortcut=QKeySequence.StandardKey.Save)
-        self.addSeparator()
-        self.addAction('Выход', self._on_exit)
-
+        self.addAction(str(), self._on_open_file,
+                       shortcut=QKeySequence.StandardKey.Open)
+        self.save_action: QAction = self.addAction(str(), self._on_save_file,
+                                                   shortcut=QKeySequence.StandardKey.Save)
         self.save_action.setEnabled(False)
+
+        self.addSeparator()
+        self.addAction(str(), self._on_exit)
+
+        self.apply_language(self.main_window.settings.value('language'))
 
     def _on_open_file(self) -> None:
         last_directory = self.main_window.settings.value('last_directory')
 
         file_path, _ = QFileDialog.getOpenFileUrl(
-            caption='Открыть файл',
+            caption=QCoreApplication.translate('File dialog', 'Open file'),
             filter='Fmg (*.fmg)',
             dir=last_directory
         )
@@ -76,7 +187,7 @@ class FileMenu(QMenu):
 
         central_widget = QWidget()
         layout = QVBoxLayout(central_widget)
-        self.table = Table()
+        self.table = Table(self.main_window)
         layout.addWidget(self.table)
         self.main_window.setCentralWidget(central_widget)
 
@@ -107,12 +218,31 @@ class FileMenu(QMenu):
     def set_save_action_enabled(self) -> None:
         self.save_action.setEnabled(True)
 
+    def apply_language(self, language_code: str) -> None:
+        translator = self.main_window.translator
+        QApplication.removeTranslator(translator)
+
+        if translator.load(f'translations\\{language_code}.qm'):
+            QApplication.installTranslator(translator)
+            self.retranslate_menu()
+
+    def retranslate_menu(self) -> None:
+        self.setTitle(QCoreApplication.translate(
+            'File menu', '&File'))
+        file_actions = self.actions()
+        file_actions[0].setText(QCoreApplication.translate(
+            'File menu', '&Open'))
+        file_actions[1].setText(QCoreApplication.translate(
+            'File menu', '&Save'))
+        file_actions[3].setText(QCoreApplication.translate(
+            'File menu', '&Exit'))
+
 
 class Table(QTableWidget):
-    def __init__(self) -> QTableWidget:
-        super().__init__()
+    def __init__(self, parent: MainWindow) -> QTableWidget:
+        super().__init__(parent)
+        self.main_window = parent
         self.setColumnCount(2)
-        self.setHorizontalHeaderLabels(['ID', 'Текст'])
         self.verticalHeader().setVisible(False)
         self.horizontalHeader().setMinimumSectionSize(40)
         self.horizontalHeader().setFixedHeight(20)
@@ -129,6 +259,8 @@ class Table(QTableWidget):
         self.setStyleSheet('QHeaderView::section{background-color:Snow}')
         self.setItemDelegate(PlainTextEditDelegate(self))
 
+        self.apply_language(self.main_window.settings.value('language'))
+
     def load_data_from_file(self, file_path: str) -> None:
         data = read_file(file_path)
 
@@ -141,20 +273,32 @@ class Table(QTableWidget):
             self.setItem(row_index, 0, id)
             self.setItem(row_index, 1, text)
 
+    def apply_language(self, language_code: str) -> None:
+        translator = self.main_window.translator
+        QApplication.removeTranslator(translator)
+
+        if translator.load(f'translations\\{language_code}.qm'):
+            QApplication.installTranslator(translator)
+            self.retranslate_table()
+
+    def retranslate_table(self) -> None:
+        self.setHorizontalHeaderLabels(
+            ['ID', QCoreApplication.translate('Table headers', 'Text')])
+
 
 class PlainTextEditDelegate(QStyledItemDelegate):
-    def createEditor(self, parent, option, index):
+    def createEditor(self, parent, option, index) -> QPlainTextEdit:
         editor = QPlainTextEdit(parent)
         editor.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAsNeeded)
         editor.keyPressEvent = self._create_key_press_handler(editor)
 
         return editor
 
-    def setEditorData(self, editor: QPlainTextEdit, index: QModelIndex):
+    def setEditorData(self, editor: QPlainTextEdit, index: QModelIndex) -> None:
         value = index.data(Qt.ItemDataRole.EditRole)
         editor.setPlainText(str(value))
 
-    def setModelData(self, editor: QPlainTextEdit, model: QAbstractItemModel, index: QModelIndex):
+    def setModelData(self, editor: QPlainTextEdit, model: QAbstractItemModel, index: QModelIndex) -> None:
         old_value: str = index.data(Qt.ItemDataRole.EditRole)
         new_value = editor.toPlainText()
 
@@ -166,10 +310,8 @@ class PlainTextEditDelegate(QStyledItemDelegate):
             if not ('•' in main_window.windowTitle()):
                 main_window.add_dot_to_window_title()
             menu.set_save_action_enabled()
-        else:
-            print('Same')
 
-    def updateEditorGeometry(self, editor: QPlainTextEdit, option, index):
+    def updateEditorGeometry(self, editor: QPlainTextEdit, option, index) -> None:
         rect: QRect = option.rect
         rect.setHeight(rect.height() + 10)
         editor.setGeometry(rect)
@@ -177,7 +319,7 @@ class PlainTextEditDelegate(QStyledItemDelegate):
     def _create_key_press_handler(self, editor: QPlainTextEdit):
         original_key_press_event = editor.keyPressEvent
 
-        def custom_key_press_event(event):
+        def custom_key_press_event(event) -> None:
             if event.key() == Qt.Key.Key_Return or event.key() == Qt.Key.Key_Enter:
                 self.commitData.emit(editor)
                 self.closeEditor.emit(
@@ -187,10 +329,3 @@ class PlainTextEditDelegate(QStyledItemDelegate):
             original_key_press_event(event)
 
         return custom_key_press_event
-
-
-class LanguageMenu(QMenu):
-    def __init__(self) -> QMenu:
-        super().__init__('&Язык')
-        self.addAction('Английский')
-        self.addAction('Русский')
